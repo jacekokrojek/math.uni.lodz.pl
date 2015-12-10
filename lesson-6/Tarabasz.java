fd45581f60d7ab59ca225a49028e5e92dac7cad4
michaltarabasz <m.tarabasz@wp.pl>
2015-12-10 14:32:12 +0100

    Lesson 6
    Java, Log, Html

---
+++ lesson-6/Tarabasz.java
@@ -0,0 +1,84 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Test {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8080/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void test() throws Exception {
+    driver.get(baseUrl + "registrationform");
+    driver.findElement(By.name("firstName")).clear();
+    driver.findElement(By.name("firstName")).sendKeys("Michał");
+    driver.findElement(By.name("lastName")).clear();
+    driver.findElement(By.name("lastName")).sendKeys("Tarabasz");
+    // ERROR: Caught exception [ERROR: Unsupported command [getEval |  | ]]
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys(email);
+    driver.findElement(By.name("repeatEmail")).clear();
+    driver.findElement(By.name("repeatEmail")).sendKeys(email);
+    driver.findElement(By.xpath("(//button[@class='gwt-Button' and text()='SQL'])")).click();
+    // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
+    // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
+    driver.findElement(By.xpath("(//button[@class='gwt-Button' and text()='Save'])")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
